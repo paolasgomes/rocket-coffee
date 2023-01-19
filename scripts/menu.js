744f46616bfb0c4066e98574af5f5f62599a2a8b
Paola Gomes <paolasgomesredatora@hotmail.com>
2023-01-19 11:31:10 -0300

    fix: close button menu mobile

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,8 +1,15 @@
 const buttonMenu = document.querySelector(".menu-toggle");
 const menu = document.querySelector(".menu-mobile");
+const imageMenu = document.querySelector(".menu-toggle img");
 
 const handleMenu = (e) => {
   menu.classList.toggle("active");
+
+  if (menu.classList.contains("active")) {
+    imageMenu.src = "../assets/menu-buguer-close.svg";
+  } else {
+    imageMenu.src = "../assets/menu-buguer-open.svg";
+  }
 };
 
 buttonMenu.addEventListener("click", handleMenu);
